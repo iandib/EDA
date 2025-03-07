2c8faebdea6ea649f3fe8b9a13f84f6a3900f5aa
iandib <ian.dib@outlook.com>
2025-03-07 18:27:19 -0300

    Added the starter code files by Marc S. Ressl

---
+++ build/CMakeFiles/orbitalsim.dir/main.cpp.obj.d
@@ -0,0 +1,12 @@
+CMakeFiles/orbitalsim.dir/main.cpp.obj: \
+ E:\3C\EDA\Practice\Repo\Starter\ Code\main.cpp \
+ E:\3C\EDA\Practice\Repo\Starter\ Code\OrbitalSim.h \
+ E:\3C\EDA\Practice\Repo\Starter\ Code\View.h \
+ C:/msys64/mingw64/include/raylib.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ C:/msys64/mingw64/include/vadefs.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h
---
+++ build/CMakeFiles/orbitalsim.dir/view.cpp.obj.d
@@ -0,0 +1,19 @@
+CMakeFiles/orbitalsim.dir/view.cpp.obj: \
+ E:\3C\EDA\Practice\Repo\Starter\ Code\view.cpp \
+ C:/msys64/mingw64/include/time.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/include/sys/timeb.h \
+ C:/msys64/mingw64/include/sec_api/sys/timeb_s.h \
+ C:/msys64/mingw64/include/_timeval.h \
+ C:/msys64/mingw64/include/pthread_time.h \
+ C:/msys64/mingw64/include/pthread_compat.h \
+ E:\3C\EDA\Practice\Repo\Starter\ Code\View.h \
+ C:/msys64/mingw64/include/raylib.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/14.2.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ E:\3C\EDA\Practice\Repo\Starter\ Code\OrbitalSim.h
