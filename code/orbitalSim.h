1d442b43ab2b10dcf14c93d359d9fc8b5b34c02e
LucianoCordero <148829453+LucianoCordero@users.noreply.github.com>
2025-03-21 17:41:38 -0300

    update1 orbitalSim.h
    copie el del att3

--- code/orbitalSim.h
+++ code/orbitalSim.h
@@ -1,50 +1,48 @@
-﻿/* *****************************************************************
-    * FILE INFORMATION *
-   ***************************************************************** */
-   
-/// @brief Orbital simulation
-/// @author Marc S. Ressl, Ian A. Dib, Luciano S. Cordero
-/// @copyright Copyright (c) 2022-2023
+#define ORBITALSIM_H/**
+/* * @brief Orbital simulation
+ * @author Marc S. Ressl
+ *
+ * @copyright Copyright (c) 2022-2023
+ */
+
+#ifndef ORBITALSIM_H
 
 
-/* *****************************************************************
-    * HEADER CONFIGURATION *
-   ***************************************************************** */
 
-   #ifndef ORBITALSIM_H
-   #define ORBITALSIM_H
-   
-   
-    //* CONSTANTS & STRUCTURES
-   
-    /// @brief Orbital body definition
-    struct OrbitalBody
-    {
-        // TODO Complete the structure definition
+#include "raylib.h"
+#include "raymath.h"
+/**
+ * @brief Orbital body definition
+ */
+struct OrbitalBody
+{
+    Vector3 position;
+    Vector3 speed;
+    Vector3 acceleration;
+    float mass;
+    float radius;
+    Color color;
 
+};
 
-    };
+/**
+ * @brief Orbital simulation definition
+ */
+struct OrbitalSim
+{
+    // Fill in your code here...
+    float timeStep;
+    float bodyCounter;
+    OrbitalBody (*body)[9];
 
+};
 
-    /// @brief Orbital simulation definition
-    struct OrbitalSim
-    {
-        // TODO Complete the structure definition
+OrbitalSim *constructOrbitalSim(float timeStep);
+void destroyOrbitalSim(OrbitalSim *sim);
 
+void updateOrbitalSim(OrbitalSim *sim);
 
-    };
-
-
-    //* PUBLIC FUNCTIONS PROTOTYPES
-
-    OrbitalSim *constructOrbitalSim(float timeStep);
-    void destroyOrbitalSim(OrbitalSim *sim);
-    void updateOrbitalSim(OrbitalSim *sim);
-
-
-    #endif // ORBITALSIM_H
-
-
+#endif
 
 
 
